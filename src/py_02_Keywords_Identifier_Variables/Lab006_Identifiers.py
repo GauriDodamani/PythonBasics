67379c0ddee511b25dfd02b2e3d737185e015c3c
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-26 19:21:54 +0530

    Identifiers

---
+++ src/py_02_Keywords_Identifier_Variables/Lab006_Identifiers.py
@@ -0,0 +1,32 @@
+#Identifier- Names given to variables, functions, classes, etc.
+# It is a variable name
+#underscore is allowed asvariable name
+
+_=5
+print(_)
+_=_+1
+print(_)
+
+#Alphanumeric are allowed as variable name
+
+abc123=45
+print(abc123)
+
+#underscore with alpha
+
+_age=23
+print(_age)
+
+#Digit is not allowed as 1st variable name
+'''
+123abc=3
+print(123abc)
+'''
+#No special character and keywords allowed
+'''@=23
+print(@)'''
+
+'''
+if=12
+print(if)
+'''
