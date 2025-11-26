4b713cde190f39db44a6620a013688ab859eb049
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-26 16:48:52 +0530

    keywords list

---
+++ src/py_02_Keywords_Identifier_Variables/Lab004_Keywords.py
@@ -0,0 +1,14 @@
+#keywords- pre named/ reserved words
+
+import keyword
+print(keyword.kwlist)
+
+'''
+List of python keywords:
+
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 
+'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 
+'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 
+'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+'''
