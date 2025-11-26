1159e73df61ee9cd423adf1eb2bb15f804f2d66d
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-26 14:34:06 +0530

    comments

---
+++ src/py_01_Python_Basic/Lab002_Python_comment.py
@@ -0,0 +1,10 @@
+#----This is used as a single line comment
+#-> it is not executed or no output will be displayed in console.
+#print("Hello world this is my first program")
+
+'''
+For
+multiple
+line
+comment(""")
+'''
