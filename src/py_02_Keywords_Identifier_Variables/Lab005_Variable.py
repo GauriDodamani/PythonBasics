7f90d1dd1ea84b67f67c5f21be207a44dd77ac8c
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-26 19:11:16 +0530

    variables

---
+++ src/py_02_Keywords_Identifier_Variables/Lab005_Variable.py
@@ -0,0 +1,24 @@
+# interger variable
+
+age=25
+print(age)
+
+# string variable
+name="Gauri"
+print("My name is", name)
+
+#assigning age value in integer than in string value gets override
+
+age=25
+age="Gauri"
+
+print(age)
+
+# assigning multiple variable and value
+a,b,c=1,2,3
+print("Value for a,b,c variable",a,b,c)
+
+#assigning same value for multiple variable
+print(end='\n')
+a=b=c="Gauri"
+print("Value for a,b,c is : ",a,b,c)
