6fe076e33a28ffa98ca41f4f08881c28289b2c92
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-26 18:27:15 +0530

    true false non

---
+++ src/py_02_Keywords_Identifier_Variables/Lab004_Keywords_if_elif_else.py
@@ -0,0 +1,49 @@
+# keywords- if, elif, else
+#if
+
+age=23
+
+if age > 18:
+    print("Entered value is true and age is",age)
+
+print("Entered value is false")
+
+
+i = 10
+
+# Checking if i is greater than 15
+if i > 15:
+    print("10 is less than 15")
+
+print("I am Not in if")
+
+
+#if-else
+
+age=35
+if age<20:
+    print("Im looking for a job")
+else:
+    print("Im working")
+
+
+gender="F"
+
+if gender=="F":
+    print("I am Female")
+else:
+    print("I am Male")
+
+
+#if-elif-else
+
+marks=16
+
+if marks >=30 and marks <=50 :
+    print("Your grade is C ")
+elif marks >=51 and marks <=80 :
+    print("Your grade is B")
+elif marks >=81 and marks <=100:
+    print("Your grade is A")
+else:
+    print("Your are failed")
