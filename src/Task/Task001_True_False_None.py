3e44231cfcc51f8afc8b97d8b9a21f16e9667de8
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-26 17:39:44 +0530

    true false none

---
+++ src/Task/Task001_True_False_None.py
@@ -0,0 +1,20 @@
+# value keywords- True, False, None
+#True , False re boolen data type
+
+x=True
+y=False
+print("Value of x is",x)
+print("Value of y is",y)
+'''o/p:Value of x is True
+       Value of y is False
+'''
+
+#None - is a null value or variable that has no object value.
+# It is a class 'NoneType'
+
+print(type(None))
+#o/p:<class 'NoneType'>
+
+var=None
+print("The variable value is",var)
+#O/P: The variable value is None
