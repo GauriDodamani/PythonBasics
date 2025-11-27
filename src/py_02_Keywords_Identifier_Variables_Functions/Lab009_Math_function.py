3c065ae6128791fc1c874298ec0e74ea470355f8
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-27 16:01:11 +0530

    math function

---
+++ src/py_02_Keywords_Identifier_Variables_Functions/Lab009_Math_function.py
@@ -0,0 +1,36 @@
+#addition example
+a=20
+b=60
+c=a+b
+print(c)
+#o/p:80
+
+#subtraction example
+c=c-30
+print(c)
+#o/p:50
+
+#bodmas
+a=23*59+2-32%4
+print(a)
+#o/p:1359
+
+#it has min & max builtins function
+#min function: finding min value
+
+a=min(45,78,32,12,4)
+print("The lowest/ minimun value is :",a)
+#o/p: The lowest/ minimun value is : 4
+
+result=max(90,67,1001,567,9237)
+print(result)
+#o/p:9237
+
+
+
+
+
+
+
+
+
