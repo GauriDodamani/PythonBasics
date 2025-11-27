d860e53e74d02dd1d112d2d76428bdf90800346f
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-27 16:06:58 +0530

    no concentation

---
+++ src/py_02_Keywords_Identifier_Variables_Functions/Lab010_no_concentation.py
@@ -0,0 +1,14 @@
+#In Python concentation is not used
+
+#print("gauri" + 9)
+'''
+O/p: error will throw
+
+print("gauri" + 9)
+          ~~~~~~~~^~~
+TypeError: can only concatenate str (not "int") to str'''
+
+
+print("gauri" , int(12))
+
+#o/p: gauri 12
