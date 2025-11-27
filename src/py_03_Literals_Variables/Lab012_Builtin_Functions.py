faabcb5a52e4e595dae9a25330e6e2ee1b855330
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-28 01:06:35 +0530

    builtin funct

---
+++ src/py_03_Literals_Variables/Lab012_Builtin_Functions.py
@@ -0,0 +1,23 @@
+#builtin functions are pre-defined by python to easily write of code
+'''
+Some of the built in :
+
+max()
+min()
+pow()
+abs()-> absoulte
+all()
+any()
+float()
+input()
+list()
+etc
+'''
+
+
+a=pow(2,3)
+print("The power of 2 is:",a)
+print(type(a))
+
+#o/p:The power of 2 is: 8
+#    <class 'int'>
