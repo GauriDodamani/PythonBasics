5f6aa0c4e6dc69e8e5621dcb2a53f0320b3fa4a9
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-27 16:00:58 +0530

    dynamically typed

---
+++ src/py_02_Keywords_Identifier_Variables_Functions/Lab008_Dynamically_Typed.py
@@ -0,0 +1,9 @@
+#It can change the variable data type at run time
+
+age=23
+print(type(age))
+age="Gauri"
+print(type(age))
+#o/p:<class 'int'>
+#    <class 'str'>
+#first interger data type was given after added string variable value it returned as str data typed.
