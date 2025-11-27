d2030bbf289d26d0c9d28e2f073117885250e8be
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-28 01:06:15 +0530

    literals

---
+++ src/py_03_Literals_Variables/Lab011_Literals type.py
@@ -0,0 +1,42 @@
+#Literals: it is a fixed value into the source code
+#interger literal
+age=65
+age+1
+print(age)
+
+#o/p: 65
+
+#string literal
+
+a= 'How r u ?'  #single quote
+b= "gauri"      #double quote
+c= '''Where r u from ? ''' #Triple quote
+
+print(a,b,c)
+#o/p: How r u ? gauri Where r u from ?
+
+print(a)
+print(b)
+print(c)
+'''
+o/p:
+How r u ?
+gauri
+Where r u from ? '''
+
+#float
+
+a=50.23
+print(a)
+
+#boolen
+
+a=True
+b=False
+print(a,b)  #True False
+print(1==True) #True
+print(0==True) #False
+print(0==False) #True 0==0-> true
+print(True+2)  #3
+print(False+6) #6
+
