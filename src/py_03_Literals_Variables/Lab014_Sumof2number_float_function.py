542d7b021ecfee0e1ae325c0426d5ebbb854f0f5
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-28 01:07:06 +0530

    float funct

---
+++ src/py_03_Literals_Variables/Lab014_Sumof2number_float_function.py
@@ -0,0 +1,31 @@
+#float()-> whatever the i/p no. it should get converted into float
+
+#Take a user input for 2 numbers and give result in sum, mul, div
+
+#i/p= n1,n2
+#o/p= sum, mul, div
+
+n1=float(input("enter n1 :"))
+n2=float(input("enter n2 :"))
+print(n1+n2)
+print(n1*n2)
+print(n1/n2)
+print(n1-n2)
+print(type(n1+n2))
+
+'''
+o/p:
+enter n1 :50
+enter n2 :25
+75.0
+1250.0
+2.0
+25.0
+<class 'float'>
+
+'''
+
+
+a=float(input("number :"))
+print(a)
+print(type(a))
