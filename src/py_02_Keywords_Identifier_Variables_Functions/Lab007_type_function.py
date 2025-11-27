8c471785c37f37d16f9f2c64c536c3138ec7d0cf
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-27 16:00:32 +0530

    type function

---
+++ src/py_02_Keywords_Identifier_Variables_Functions/Lab007_type_function.py
@@ -0,0 +1,38 @@
+#type is a function which identify what data type is for var value
+
+age= 34
+print(type(age))
+
+#o/p:<class 'int'>
+
+name="Gauri"
+print(type(name))
+
+#o/p:<class 'str'>
+
+is_value=True
+print(type(is_value))
+
+#o/p:<class 'bool'>
+
+pi=3.14
+print(type(pi))
+
+#o/p:<class 'float'>
+
+#Complpex_number: it is also a function where it gives real and imaginary values details
+#not used in automation
+
+complex_number=2+5j
+print(type(complex_number))
+print(complex_number)
+print(complex_number.real)
+print(complex_number.imag)
+
+'''
+o/p:
+<class 'complex'>
+(2+5j)
+2.0
+5.0
+'''
