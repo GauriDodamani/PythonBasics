396ed7ca4cc42e1c8b5fe4a9928ce5ee45d35a97
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-28 01:06:50 +0530

    user i/p

---
+++ src/py_03_Literals_Variables/Lab013_User_Input.py
@@ -0,0 +1,19 @@
+#input()- O/p gets converted to string
+#whatever i/p is o/p is always in string data type
+
+a=input("Enter your name : ")
+print(a)
+print(type(a))
+
+'''
+Enter your name : Gauri
+Gauri
+<class 'str'>
+'''
+
+'''
+Enter your name : 123
+123
+<class 'str'>
+'''
+
