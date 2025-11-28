ef83808e79d34df9d96f804137496750f5d97091
GauriDodamani <dodamanigauri9@gmail.com>
2025-11-28 17:57:02 +0530

    arithmetic operators

---
+++ src/py_04_Operators/Lab020_Arithmetic_operators.py
@@ -0,0 +1,27 @@
+#Operators: Special symbols use to operate variable or values.
+
+#Arithmetic operator : +,-,*,/,//,%,**
+
+#Take input from user
+
+n1=int(input("Enter 1st no : "))
+n2=int(input("Enter 2nd no : "))
+
+add=n1+n2
+print(add)
+
+sub=n1-n2
+print(sub)
+
+mul=n1*n2
+print(mul)
+
+div=n1/n2
+print(div)
+
+quotient=n1//n2
+print(quotient)
+
+reminder=n1%n2
+print(reminder)
+
