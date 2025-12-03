71152b64971b1b67f325ad05cfd0ca94ae24b2e9
GauriDodamani <dodamanigauri9@gmail.com>
2025-12-03 20:43:14 +0530

    loop

---
+++ src/Task/task_0_18_oct_if_else_for_whileloop/task_011_page_loading.py
@@ -0,0 +1,10 @@
+# Simulate a page loading check using a while loop.
+# If page_loaded becomes True within 5 seconds, print success; else timeout.
+# Hint: Use a counter (like wait_time) and break condition.
+
+
+#logic:
+# wait_time=1
+#
+# while wait_time == 5:
+#
