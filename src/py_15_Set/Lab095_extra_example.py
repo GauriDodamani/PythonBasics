76bac99be992ddae29b5bf32db8296dee17419ec
GauriDodamani <dodamanigauri9@gmail.com>
2025-12-10 00:21:43 +0530

    set

--- src/py_15_Set/Lab095_extra_example.py
+++ src/py_15_Set/Lab095_extra_example.py
@@ -4,17 +4,20 @@
 # print(square)
 # # {0, 1, 4, 9, 16}
 
+
 squ = []
 for x in range (5):
-    squ= x ** 2
-    result=set(squ)
-    print(result)
+    squ.append(x ** 2)
 
-'''
-0
-1
-4
-9
-16
+result=set(squ)
+print(result)
+# {0, 1, 4, 9, 16}
 
-'''
+
+# frozenset()-Immutable/ Unchangeable and removes duplicate element
+# Can't be changed after creation
+
+fset = frozenset([1,2,3,4,5,3,6,7,3,5,8])
+print(fset)
+
+#  frozenset({1, 2, 3, 4, 5, 6, 7, 8})
